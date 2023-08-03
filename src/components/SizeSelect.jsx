1e5168c12077d1988c821a320c6edf0b413d08e7
Kylie Oliver <olivkylie@gmail.com>
2023-08-02 22:45:47 -0400

    built product page

---
+++ src/components/SizeSelect.jsx
@@ -0,0 +1,26 @@
+import "./ProductPage.css"
+
+const SizeSelect = () => {
+  return (
+    <ul className="size-list">
+        <li>6</li>
+        <li>6.5</li>
+        <li>7</li>
+        <li>7.5</li>
+        <li>8</li>
+        <li>8.5</li>
+        <li>9</li>
+        <li>9.5</li>
+        <li>10</li>
+        <li>10.5</li>
+        <li>11</li>
+        <li>11.5</li>
+        <li>12</li>
+        <li>12.5</li>
+        <li>13</li>
+        <li>14</li>
+    </ul>
+  )
+}
+
+export default SizeSelect
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,50 @@
 import { useParams } from "react-router"
+import { shoes } from "../ShoeData";
+import "../components/ProductPage.css"
+import SizeSelect from "../components/SizeSelect";
+import { useState } from "react";
 
 const ProductPage = () => {
   const {id} = useParams();
-  console.log(id);
+  const featuredShoe = shoes.find( shoe => shoe.id === parseInt(id) )
+
+  const [selectedSize, setSelectedSize] = useState()
 
   return (
-    <div>ProductPage</div>
+    <div className="product-page-container">
+      <div className="product-container">
+        <div className="product-images">
+          <div className="product-header-mobile">
+            <h5 className="product-name">{featuredShoe.brand} {featuredShoe.name}</h5>
+            <p className="product-price">${featuredShoe.price}</p>
+          </div>
+          <div className="image-reel">
+            <img src={featuredShoe.img} className="image-thumbnail"/>
+            {featuredShoe.otherImgs.map((image) => (
+              <img key={image} src={image} className="image-thumbnail"/>
+            ))}
+          </div>
+          <img src={featuredShoe.img} className="main-image" />
+        </div>
+        <div className="product-right">
+          <div className="product-header">
+            <h5 className="product-name">{featuredShoe.brand} {featuredShoe.name}</h5>
+            <p className="product-price">${featuredShoe.price}</p>
+          </div>
+          <div className="size-container">
+            <h6>Select Size</h6>
+            <SizeSelect selectedSize={selectedSize} setSelectedSize={setSelectedSize} />
+          </div>
+          <div className="purchase-btns">
+            <button>Add to Cart</button>
+            <button>Buy Now</button>
+          </div>
+          <p>Shipping* <br/> To get accurate shipping information Edit Location</p>
+          <p className="product-description"> <strong>Description: </strong>{featuredShoe.description}</p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
